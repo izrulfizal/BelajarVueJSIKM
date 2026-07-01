1b2b3023ab58d746fa3967ede0808d5569a8160b
izrulfizal <izrulfizal1410@gmail.com>
2026-07-01 13:03:29 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloMessage from './components/HelloMessage.vue';
+
+</script>
+
+<template>
+
+<HelloMessage msg="Welcome to Your Vue.js App" />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloMessage.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+defineProps({
+  nama: {
+    type: String,
+    default: 'TVETMARA Besut'
+  }
+})
+
+</script>
+
+<template>
+  <div class="box">
+    <h1>Hello!</h1>
+    <h3>{{ nama }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.box{
+    border: 2px dashed white;
+    padding: 30px;
+    border-radius: 30px;
+    background-color:cadetblue;
+    margin: 30px;
+}
+</style>
